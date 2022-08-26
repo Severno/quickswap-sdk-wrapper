527ac4e4c71f3eb2e4040eb79f04152fbfbdfcef
Artem Bykov <mokijoy@gmail.com>
2022-08-26 17:32:53 +0400

    Update scripts

--- src/getPath.ts
+++ src/getPath.ts
@@ -1,10 +1,10 @@
-const {ChainId, Fetcher, Trade, JSBI, Pair, Percent, Token, TokenAmount, WETH} = require("quickswap-sdk");
-import {basePairs, bases, GlobalData } from "./constants";
+import {ChainId, Fetcher, Pair, Token, TokenAmount, Trade} from "quickswap-sdk";
 import {provider} from "./provider";
+import {basePairs, bases, GlobalData} from "./constants";
 
 const fs = require('fs')
 
-
+const args = process.argv.slice(2);
 
 async function getTokenData(tokenAddress: string) {
   return await Fetcher.fetchTokenData(ChainId.MATIC, tokenAddress, provider)
@@ -58,27 +58,23 @@ function getPairsCombinations(tokenA, tokenB) {
     : []
 }
 
-const getPairs = async (pairs) => {
-  const newPairs = await pairs.reduce(async (accumP, current, index) => {
+const getPairs = async (pairs, tokenAmount) => {
+  return await pairs.reduce(async (accumP, current, index) => {
     const accum = await accumP;
     let pair
     try {
       pair = await Fetcher.fetchPairData(current[0], current[1], provider)
     } catch (e) {
       pair = new Pair(
-        new TokenAmount(current[0], '1000000000000000'),
-        new TokenAmount(current[1], '1000000000000000'),
+        new TokenAmount(current[0], tokenAmount),
+        new TokenAmount(current[1], tokenAmount),
       )
     }
 
-
     return [...accum, pair]
 
-  }, Promise.resolve([]));
-
-  return newPairs
+  }, Promise.resolve([]))
 }
-
 
 const MAI_ADDRESS = '0xa3Fa99A148fA48D14Ed51d610c367C61876997F1'
 const AAVE_ADDRESS = '0xd6df932a45c0f255f85145f286ea0b292b21c90b'
@@ -87,59 +83,46 @@ const DAI_ADDRESS = '0x8F3CF7AD23CD3CADBD9735AFF958023239C6A063'
 const USDC_ADDRESS = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
 const ACRE_ADDRESS = '0x011734f6Ed20E8D011d85Cf7894814B897420acf'
 
-// const ETH = await getTokenData(ETH_ADDRESS)
-// const AAVE = await getTokenData(AAVE_ADDRESS)
-// const DAI = await getTokenData(DAI_ADDRESS)
-// const USDC = await getTokenData(USDC_ADDRESS)
-// const MAI = await getTokenData(MAI_ADDRESS)
-// const ACRE = await getTokenData(ACRE_ADDRESS)
+async function bestTradeExactIn(pairs, tokenA, tokenB, tokenAToSell) {
+  const newPairs = await getPairs(pairs, tokenAToSell)
 
-// const PAIR_MAI_ACRE = new qs.Pair(new qs.TokenAmount(MAI, '2000000000000000000'), new qs.TokenAmount(ACRE, '1000000000000000000'))
-
-
-// const PAIR_MAI_ACRE = await getPairData(MAI, ACRE)
-
-// const PAIR_ETH_DAI = await getPairData(AAVE, ETH)
-
-async function getPath1(pairs, tokenA, tokenB, tokenAToSell) {
-  // console.log('pairs', pairs)
-  const newPairs = await getPairs(pairs)
-
-  return Trade.bestTradeExactIn(newPairs, tokenAToSell, tokenB, {
+  return Trade.bestTradeExactIn(newPairs, new TokenAmount(tokenA, tokenAToSell), tokenB, {
     maxHops: 3,
     maxNumResults: 1,
   })[0] ?? []
 }
 
+const file = fs.createWriteStream('getPathResult.txt');
 
-// console.log('getPairs(MAI, ACRE)', getPairs(MAI, ACRE))
+const getPath = async () => {
+  const [tokenARaw, tokenBRaw, tokenAMount] = args
 
-const file = fs.createWriteStream('array.txt');
-const newFile = fs.createWriteStream('newArray.txt');
+  const tokenAData = JSON.parse(tokenARaw)
+  const tokenBData = JSON.parse(tokenBRaw)
 
-const writeToFile = (arr: any[]) => {
-  arr.forEach((v) => {
-    file.write(JSON.stringify(v) + ',' +'\n');
-  });
+  const tokenA = new Token(tokenAData.chainId, tokenAData.address, tokenAData.decimals, tokenAData.symbol, tokenAData.name)
+  const tokenB = new Token(tokenBData.chainId, tokenBData.address, tokenBData.decimals, tokenBData.symbol, tokenBData.name)
 
-  file.end();
-}
-// @ts-ignore
-const runPath = async () => {
-  const MAI = await getTokenData(MAI_ADDRESS)
-  const ACRE = await getTokenData(ACRE_ADDRESS)
+  const bestTradeExactInResult = await bestTradeExactIn(getPairsCombinations(tokenA, tokenB), tokenA, tokenB, tokenAMount)
 
-  console.log('MAI:', MAI, ACRE, getPairsCombinations(MAI, ACRE))
-
-
-  writeToFile(getPairsCombinations(MAI, ACRE))
-
-
-  const ppp = await getPath1(getPairsCombinations(MAI, ACRE), MAI, ACRE, new TokenAmount(MAI, '1000000000000000'))
-  console.log('ppp', ppp.route.path)
+  file.write(JSON.stringify(bestTradeExactInResult))
+  console.log('ppp', JSON.stringify(bestTradeExactInResult))
 }
 
+getPath()
 
-runPath()
+const tokenA = {
+  decimals: 18,
+  symbol: 'WMATIC',
+  name: 'Wrapped Matic',
+  chainId: 137,
+  address: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+}
 
-
+const tokenB = {
+  decimals: 18,
+  symbol: 'QUICK(OLD)',
+  name: 'Quickswap(OLD)',
+  chainId: 137,
+  address: '0x831753DD7087CaC61aB5644b308642cc1c33Dc13',
+}
--- src/provider.ts
+++ src/provider.ts
@@ -1,4 +1,4 @@
-const {ethers} = require('ethers')
+import {ethers} from 'ethers';
 
 const alchemy = 'https://polygon-mainnet.g.alchemy.com/v2/qteyaTH9FM30hxyZ1iPHbRLYCb7GG8pM'
 const provider = new ethers.providers.JsonRpcProvider(alchemy)
